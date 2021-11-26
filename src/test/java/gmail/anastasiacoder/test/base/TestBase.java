353447725a4f1f891b3d5fdd1d6ff1c9d3307bc0
AnastasiaCoder <mstitelnica1999@gmail.com>
2021-11-26 16:13:32 +0700

    added listener

--- src/test/java/gmail/anastasiacoder/test/base/TestBase.java
+++ src/test/java/gmail/anastasiacoder/test/base/TestBase.java
@@ -1,12 +1,15 @@
 package gmail.anastasiacoder.test.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1920x1080";
     }
 }
